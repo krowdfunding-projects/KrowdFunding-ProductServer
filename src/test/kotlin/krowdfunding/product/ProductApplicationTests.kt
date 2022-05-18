61b1dfcd9ba74b4570b69369cc8ade63cd4200ad
Junseong kim <anima94@kakao.com>
2022-05-18 21:56:18 +0900

    test: 실패하는 테스트 케이스 설정.

--- src/test/kotlin/krowdfunding/product/ProductApplicationTests.kt
+++ src/test/kotlin/krowdfunding/product/ProductApplicationTests.kt
@@ -1,6 +1,8 @@
 package krowdfunding.product
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
@@ -8,6 +10,7 @@ class ProductApplicationTests {
 
     @Test
     fun contextLoads() {
+        fail("테스트 케이스가 실패해야함.")
     }
 
 }
