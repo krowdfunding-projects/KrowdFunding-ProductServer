51b02f309df61549e0f8bc4862bd2e4531f26c38
Junseong kim <anima94@kakao.com>
2022-05-23 11:36:16 +0900

    style : ProductRepositoryImpl querydsl 로직 포맷 변경

--- src/main/kotlin/krowdfunding/product/repository/product/ProductRepositoryImpl.kt
+++ src/main/kotlin/krowdfunding/product/repository/product/ProductRepositoryImpl.kt
@@ -2,10 +2,8 @@ package krowdfunding.product.repository.product
 
 import com.querydsl.core.types.Projections
 import com.querydsl.jpa.impl.JPAQueryFactory
-import krowdfunding.product.domain.company.QCompany
 import krowdfunding.product.domain.company.QCompany.*
 import krowdfunding.product.domain.product.QProduct.product
-//import krowdfunding.product.QProduct.*
 import krowdfunding.product.dto.ProductInfoDto
 
 class ProductRepositoryImpl(val jpaQueryFactory: JPAQueryFactory)
@@ -14,20 +12,21 @@ class ProductRepositoryImpl(val jpaQueryFactory: JPAQueryFactory)
     override fun getProductInfo(productNumber: String): ProductInfoDto? {
         return jpaQueryFactory.select(
             Projections.constructor(ProductInfoDto::class.java,
-                    product.quantity,
-                    product.content,
-                    product.collectedAmount,
-                    product.type,
-                    product.title,
-                    product.fundingEndDate,
-                    product.targetAmount,
-                    product.company.companyName,
-                    product.supporters.size()
-                )
+                product.quantity,
+                product.content,
+                product.collectedAmount,
+                product.type,
+                product.title,
+                product.fundingEndDate,
+                product.targetAmount,
+                product.company.companyName,
+                product.supporters.size()
+            )
         ).from(product)
             .leftJoin(product.company, company)
+            .distinct()
             .where(product.productNumber.eq(productNumber))
-            .fetchOne()
+            .fetchFirst()
 
     }
 }
